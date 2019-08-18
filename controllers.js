b8f93ff6cb0b754bfee6b7a61537b6ddd9ae80a2
RVhanbatte <raghuvhanbatte@gmail.com>
2019-08-18 14:20:51 +0000

    u

--- controllers.js
+++ controllers.js
@@ -2,7 +2,7 @@ var myControllers = angular.module('MyControllers', []);
 
 myControllers.controller('SearchController',
     function MyController($scope, $http){
-        $http.get('../js/data.json').then(function(res){
+        $http.get('js/data.json').then(function(res){
             $scope.artists = res.data;
             $scope.artistOrder = 'name';
         });
@@ -10,16 +10,16 @@ myControllers.controller('SearchController',
 
 myControllers.controller('DetailsController',
     function MyController($scope, $http, $routeParams){
-        $http.get('../js/data.json').then(function(res){
+        $http.get('js/data.json').then(function(res){
             $scope.artists = res.data;
             $scope.whichItem = $routeParams.itemId;
-            
+
             if($routeParams.itemId > 0){
                 $scope.previousItem = Number($routeParams.itemId) - 1;
             } else{
                 $scope.previousItem = $scope.artists.length - 1;
             }
-            
+
             if($routeParams.itemId >= $scope.artists.length-1){
                 $scope.nextItem = 0;
             } else{
